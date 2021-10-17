5c8beb74c5b0177c124d6d929f64c66d1dc69ab9
Figo Alfarqi <figoalfarqi0101@gmail.com>
2021-10-17 20:33:14 +0700

    commit ketiga

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -57,8 +57,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="admin/index3.html" class="brand-link">
-      <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Admin SisKu</span>
+      <span class="brand-text font-weight-light">Admin ProfilKu</span>
     </a>
 
     <!-- Sidebar -->
@@ -66,7 +65,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('/image/foto_public/IMG_0015putih.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Ahmad Figo Alfarqi</a>
